2b7e33791763acce65f31fa7b089141af5a916c5
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2019-10-03 22:11:22 -0400

    add LambdaFilter node for arbitrary functions applied to data
    remove unnecessary output

---
+++ gunpowder/nodes/lambda_filter.py
@@ -0,0 +1,73 @@
+import logging
+import numpy as np
+import collections
+from scipy.ndimage.morphology import distance_transform_edt
+from gunpowder.array import Array
+from gunpowder.nodes.batch_filter import BatchFilter
+
+logger = logging.getLogger(__name__)
+
+
+class LambdaFilter(BatchFilter):
+    '''Apply an aribitrary function to the data in an array.
+
+    Args:
+
+        func(``function``): Function that will be applied to the array data. The only required input should be a
+            numpy array.
+
+        source_key(:class:``ArrayKey``): The :class:``ArrayKey`` that provides the input to the function.
+
+        target_key(tuple of :class:``ArrayKey``, optional): The :class:``ArrayKey`` that the result will be written
+            to. If None, `source_key` will be overwritten. (default: None)
+
+        target_spec(:class: ``ArraySpec``, optional): The :class:``ArraySpec`` for the target key. If not given the
+            spec of source_key will be used. All members that are None will also be overwritten by spec of source_key.
+    '''
+
+    def __init__(
+            self,
+            func,
+            source_key,
+            target_key=None,
+            target_spec=None
+            ):
+        self.func = func
+        self.source_key = source_key
+        if target_key is not None:
+            self.target_key = target_key
+        else:
+            self.target_key = source_key
+        self.target_spec = target_spec
+
+    def setup(self):
+
+        assert self.source_key in self.spec, (
+            "Upstream does not provide %s needed by "
+            "LambdaFilter" % self.source_key)
+
+        if self.target_key != self.source_key:
+            if self.target_spec is not None:
+                spec = self.target_spec
+                if spec.voxel_size is None:
+                    spec.voxel_size = self.spec[self.source_key].voxel_size
+                if spec.roi is None:
+                    spec.roi = self.spec[self.source_key].roi
+                if spec.dtype is None:
+                    spec.dtype = self.spec[self.source_key].dtype
+                if spec.interpolatable is None:
+                    spec.interpolatable = self.spec[self.source_key].interpolatable
+            else:
+                spec = self.spec[self.source_key].copy()
+            self.provides(self.target_key, spec)
+
+    def process(self, batch, request):
+
+        if self.target_key not in request:
+            return
+        arr = batch.arrays[self.source_key].data
+        func_output = self.func(arr)
+
+        spec = self.spec[self.target_key].copy()
+        spec.roi = request[self.target_key].roi
+        batch.arrays[self.target_key] = Array(func_output, spec)
