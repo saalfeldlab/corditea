ca59b709a6538333823063b75458d3d2a327207b
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2019-10-17 15:39:13 -0400

    add node to crop an array

---
+++ gunpowder/contrib/nodes/crop_array.py
@@ -0,0 +1,50 @@
+from gunpowder.nodes.batch_filter import BatchFilter
+from gunpowder.coordinate import Coordinate
+from gunpowder.array import ArrayKey
+from gunpowder.array_spec import ArraySpec
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CropArray(BatchFilter):
+
+    '''Crop an array
+
+    Args:
+
+        key (:class:`ArrayKey`):
+
+            The key of the array to crop.
+
+        cropping_widths_neg (``int`` or ``tuple`` of ``int``):
+
+            The amount to cut away at the beginning.
+
+        cropping_widths_pos (``int`` or ``tuple`` of ``int``):
+
+            The amount to cut away at the end.
+    '''
+
+    def __init__(self, key, cropping_widths_neg, cropping_widths_pos):
+
+        assert isinstance(key, ArrayKey)
+        print("Cropping_widths_neg", cropping_widths_neg)
+        self.key = key
+        self.cropping_widths_neg = cropping_widths_neg
+        self.cropping_widths_pos = cropping_widths_pos
+
+    def setup(self):
+        assert self.key in self.spec, ("Asked to crop %s, but is not provided upstream."%self.key)
+        assert self.spec[self.key].roi is not None, ("Asked to crop %s, but upstream provider doesn't have a ROI for it"
+                                                      "."%self.key)
+
+    def prepare(self, request):
+        cropped_roi = request[self.key].roi.grow(Coordinate(self.cropping_widths_neg),
+                                                 Coordinate(self.cropping_widths_pos))
+
+        request[self.key].roi = cropped_roi
+
+    def process(self, batch, request):
+        request_roi = request[self.key].roi
+        batch.arrays[self.key] = batch.arrays[self.key].crop(request_roi)
