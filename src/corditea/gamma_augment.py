5965c88b806fb5c7de384bc8eb91930b2ae49970
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2024-02-06 14:53:17 -0500

    feat: :sparkles: import changes from DaCapo

--- src/corditea/gamma_augment.py
+++ src/corditea/gamma_augment.py
@@ -1,63 +1,67 @@
 import numpy as np
-import gunpowder as gp
-from collections import Iterable
 
-class GammaAugment(gp.BatchFilter):
-    '''Randomly scale and shift the values of an intensity array.
+from gunpowder.nodes.batch_filter import BatchFilter
+from collections.abc import Iterable
 
-    Args:
+import logging
 
-        array (:class:`ArrayKey`):
+logger = logging.getLogger(__file__)
 
-            The intensity array to modify.
 
-        scale_min (``float``):
-        scale_max (``float``):
-        shift_min (``float``):
-        shift_max (``float``):
-
-            The min and max of the uniformly randomly drawn scaling and
-            shifting values for the intensity augmentation. Intensities are
-            changed as::
-
-                a = a.mean() + (a-a.mean())*scale + shift
-
-        z_section_wise (``bool``):
-
-            Perform the augmentation z-section wise. Requires 3D arrays and
-            assumes that z is the first dimension.
-    '''
+class GammaAugment(BatchFilter):
+    """
+    An Augment to apply gamma noise
+    """
 
     def __init__(self, arrays, gamma_min, gamma_max):
         if not isinstance(arrays, Iterable):
-            arrays = [arrays,]
+            arrays = [
+                arrays,
+            ]
         self.arrays = arrays
         self.gamma_min = gamma_min
         self.gamma_max = gamma_max
         assert self.gamma_max >= self.gamma_min
 
+    def setup(self):
+        for array in self.arrays:
+            self.updates(array, self.spec[array])
+
     def process(self, batch, request):
-        sample_gamma_min = (max(self.gamma_min, 1./self.gamma_min) - 1)*(-1)**(self.gamma_min < 1)
-        sample_gamma_max = (max(self.gamma_max, 1./self.gamma_max) - 1)*(-1)**(self.gamma_max < 1)
+        sample_gamma_min = (max(self.gamma_min, 1.0 / self.gamma_min) - 1) * (-1) ** (
+            self.gamma_min < 1
+        )
+        sample_gamma_max = (max(self.gamma_max, 1.0 / self.gamma_max) - 1) * (-1) ** (
+            self.gamma_max < 1
+        )
         gamma = np.random.uniform(sample_gamma_min, sample_gamma_max)
         if gamma < 0:
-            gamma = 1./(-gamma+1)
+            gamma = 1.0 / (-gamma + 1)
         else:
             gamma = gamma + 1
         for array in self.arrays:
             raw = batch.arrays[array]
 
-            assert raw.data.dtype == np.float32 or raw.data.dtype == np.float64, "Gamma augmentation requires float " \
-                                                                                 "types for the raw array (not " + str(raw.data.dtype) + "). Consider using Normalize before."
-            assert raw.data.min() >= 0 and raw.data.max() <= 1, "Gamma augmentation expects raw values in [0," \
-                                                                "1]. Consider using Normalize before."
+            assert raw.data.dtype == np.float32 or raw.data.dtype == np.float64, (
+                "Gamma augmentation requires float "
+                "types for the raw array (not "
+                + str(raw.data.dtype)
+                + "). Consider using Normalize before."
+            )
 
             raw.data = self.__augment(raw.data, gamma)
 
-            # clip values, we might have pushed them out of [0,1]
-            raw.data[raw.data > 1] = 1
-            raw.data[raw.data < 0] = 0
-
     def __augment(self, a, gamma):
-
-        return a**gamma
+        # normalize a
+        a_min = a.min()
+        a_max = a.max()
+        if abs(a_min - a_max) > 1e-3:
+            # apply gamma noise
+            a = (a - a_min) / (a_max - a_min)
+            noisy_a = a**gamma
+            # undo normalization
+            noisy_a = a * (a_max - a_min) + a_min
+            return noisy_a
+        else:
+            logger.warning("Skipping gamma noise since denominator would be too small")
+            return a
